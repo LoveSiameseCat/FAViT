3360256137322b560d2a68a90edba59a374d3ab4
SiameseCat <56194433+LoveSiameseCat@users.noreply.github.com>
2024-04-08 18:01:23 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -13,14 +13,6 @@ The official jax code is released and available at https://github.com/google-res
 DeiT model defs and weights from https://github.com/facebookresearch/deit,
 paper `DeiT: Data-efficient Image Transformers` - https://arxiv.org/abs/2012.12877
 
-Acknowledgments:
-* The paper authors for releasing code and weights, thanks!
-* I fixed my class token impl based on Phil Wang's https://github.com/lucidrains/vit-pytorch ... check it out
-for some einops/einsum fun
-* Simple transformer style inspired by Andrej Karpathy's https://github.com/karpathy/minGPT
-* Bert reference code checks against Huggingface Transformers and Tensorflow Bert
-
-Hacked together by / Copyright 2020, Ross Wightman
 """
 import math
 import logging

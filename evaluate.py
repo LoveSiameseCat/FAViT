0986d36df7e176140ddf8c262dcc7c913d8fa873
SiameseCat <56194433+LoveSiameseCat@users.noreply.github.com>
2024-04-14 15:50:32 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1239,7 +1239,7 @@ if __name__ == '__main__':
 
     model = vit_base_patch16_224_in21k(pretrained=True,num_classes=2)
 
-    path = 'FFA_ViT_base.tar' # Please download the pre-trained model from the provided website link
+    path = 'FA_ViT_base.tar' # Please download the pre-trained model from the provided website link
 
     checkpoint = torch.load(path,map_location='cpu')
     model.load_state_dict(checkpoint['model_state_dict'])
